c0df3d8a89bf654c145b7172652e4040ab03d7f3
kovev1983-gif <kovev1983@gmail.com>
2026-01-03 09:25:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv('7906735196:AAE8o_KiI8RBGYaGFu6i0ucznWAVqjUibiU')
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 BACKUP_TIME = '00:00'  # 3:00 MSK (полночь UTC)
 ADMIN_IDS = list(map(int, os.getenv('ADMIN_IDS', '').split(','))) if os.getenv('ADMIN_IDS') else []
 DATABASE_NAME = 'running_club.db'
 BACKUP_NAME = 'backup_running_club.db'
+
 
